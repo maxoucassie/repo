a2148a9799f2256e0d005ae15f65b8e5c460b935
maxoucassie <maxouchobbits@outlook.fr>
2018-02-06 02:33:56 +0100

    Updates

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome</h1>
+        </header>
+        <section>
+          <p>Développeuse Front-End</p>
+        </section>
+      </div>
+    );
+  }
+}
+
+
+export default App;
